6e8e076ea9139b1502227748c3d08dd7724c491e
Andres Felipe Llamoza <andres.llamoza@misena.edu.co>
2022-07-14 20:42:12 -0500

    Verion 2 de proyecto react por llamosa

--- src/components/Cards/CardsApp.js
+++ src/components/Cards/CardsApp.js
@@ -1,19 +1,86 @@
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
+import Cod from "./cod.jpg";
+import Rdr2 from "./rdr2.jpg";
+import Elden from "./elden.jpg";
+import Terraria from "./terraria.jpg";
+import Lastofus from "./lastofus.jpg";
+import Gtav from "./gtav.jpg";
 
 function CardsApp() {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+    <>
+    <h1 className='text-center'>🔥Listas de exitos🔥</h1>
+    <Row xs={3} md={2} className="g-1">
+    <Card className='d-flex'style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Cod} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Call of duty Warzone</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          una de las ultimas entregas de franquicia de call of duty, enfrentate en un mapa contra otros jugadores y logra ser ultimo en pie para ganar 
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">Ver mas</Button>
       </Card.Body>
     </Card>
+
+    <Card ClassName='d-flex'style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Elden} />
+      <Card.Body>
+        <Card.Title>Call of duty Warzone</Card.Title>
+        <Card.Text>
+          una de las ultimas entregas de franquicia de call of duty, enfrentate en un mapa contra otros jugadores y logra ser ultimo en pie para ganar 
+        </Card.Text>
+        <Button variant="primary">Ver mas</Button>
+      </Card.Body>
+    </Card>
+    
+    <Card ClassName='d-flex'style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Gtav} />
+      <Card.Body>
+        <Card.Title>Call of duty Warzone</Card.Title>
+        <Card.Text>
+          una de las ultimas entregas de franquicia de call of duty, enfrentate en un mapa contra otros jugadores y logra ser ultimo en pie para ganar 
+        </Card.Text>
+        <Button variant="primary">Ver mas</Button>
+      </Card.Body>
+    </Card>
+
+    <Card ClassName='d-flex'style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Lastofus} />
+      <Card.Body>
+        <Card.Title>Call of duty Warzone</Card.Title>
+        <Card.Text>
+          una de las ultimas entregas de franquicia de call of duty, enfrentate en un mapa contra otros jugadores y logra ser ultimo en pie para ganar 
+        </Card.Text>
+        <Button variant="primary">Ver mas</Button>
+      </Card.Body>
+    </Card>
+    
+    <Card ClassName='d-flex'style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Rdr2} />
+      <Card.Body>
+        <Card.Title>Call of duty Warzone</Card.Title>
+        <Card.Text>
+          una de las ultimas entregas de franquicia de call of duty, enfrentate en un mapa contra otros jugadores y logra ser ultimo en pie para ganar 
+        </Card.Text>
+        <Button variant="primary">Ver mas</Button>
+      </Card.Body>
+    </Card>
+    
+    <Card ClassName='d-flex'style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={Terraria} />
+      <Card.Body>
+        <Card.Title>Call of duty Warzone</Card.Title>
+        <Card.Text>
+          una de las ultimas entregas de franquicia de call of duty, enfrentate en un mapa contra otros jugadores y logra ser ultimo en pie para ganar 
+        </Card.Text>
+        <Button variant="primary">Ver mas</Button>
+      </Card.Body>
+    </Card>
+    </Row>
+    </>
   );
 }
 
